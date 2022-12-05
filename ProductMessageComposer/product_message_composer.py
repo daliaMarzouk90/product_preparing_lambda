3d923c88823d56444cab2292b5d56ea6ba8a671a
daliaMarzouk90 <dalia.m.refaie@gmail.com>
2022-12-05 11:57:43 +0200

    remove body from skus to delete

--- ProductMessageComposer/product_message_composer.py
+++ ProductMessageComposer/product_message_composer.py
@@ -1,20 +1,25 @@
 def message_composer(product, type, variants = []):
     message = {}
 
-    __set_action(message, type)
+    message["action"] = __set_action(message, type)
+
     message["sku"] = product["sku"]
-    __set_message_body(message, product, type, variants)
+
+    if type != "to_delete":
+        message["body"] = __set_message_body(message, product, type, variants)
     
     return message
 
 def __set_action(message, type):
     if type == "to_delete":
-        message["action"] = "Delete"
+        return "Delete"
     elif type == "simple" or type == "configurable":
-        message["action"] = "Save"
+        return "Save"
 
 def __set_message_body(message, product, type, variants):
-    message["body"] = product
+    body = product
     
     if type == "configurable":
-        message["body"]["all_children"] = variants
+        body["all_children"] = variants
+
+    return body
