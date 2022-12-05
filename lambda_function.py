75a0600bacfe4790296ee1f2d06666b4537adf2d
daliaMarzouk90 <dalia.m.refaie@gmail.com>
2022-12-05 13:53:35 +0200

    attinuate with ahmed's messages

--- lambda_function.py
+++ lambda_function.py
@@ -4,8 +4,11 @@ from Controller import product_preparing_controller
 def lambda_handler(event, context):
     # TODO implement
     print("****************Start*****************************")
-    
-    products_ids = [int(x["body"]["product_id"]) for x in event["Records"]]
+    products_ids = []
+    for record in event["Records"]:
+        record_products_ids = [x["product_id"] for x in json.loads(record["body"])]
+        products_ids += record_products_ids
+
     product_preparing_controller.run("ar", products_ids)
     product_preparing_controller.run("en", products_ids)
     
